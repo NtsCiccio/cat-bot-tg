27a82b5edd401d3ce6bdbc86de8d9823840023b4
NTSciccio <re.mauro@live.com>
2021-07-13 13:22:51 +0200

    Update handlerResponse.ts
    ts typing

--- src/utils/handlerResponse.ts
+++ src/utils/handlerResponse.ts
@@ -1,11 +1,12 @@
+import { Context } from 'telegraf';
 import { getRandomCatPics, getRandomCatGif } from './httpRequests.js';
 
-export async function catPics(context) {
+export async function catPics(context: Context) {
   const url = await getRandomCatPics();
   context.replyWithPhoto({ url });
 }
 
-export async function catGifs(context) {
+export async function catGifs(context: Context) {
   const url = await getRandomCatGif();
   context.replyWithAnimation({ url });
 }
